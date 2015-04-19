a3489969d5ed3c0de64b86f41b1d002d735445f2
Mauricio de Abreu Antunes <mauricio.abreua@gmail.com>
2015-04-19 20:30:24 -0300

    Added index power
    Best solution used IndexError instead of generic exceptions. :(

---
+++ index-power.py
@@ -0,0 +1,15 @@
+def index_power(array, n):
+    """
+    Find Nth power of the element with index N.
+    """
+    try:
+        return pow(array[n], n)
+    except:
+        return -1
+​
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert index_power([1, 2, 3, 4], 2) == 9, "Square"
+    assert index_power([1, 3, 10, 100], 3) == 1000000, "Cube"
+    assert index_power([0, 1], 0) == 1, "Zero power"
+    assert index_power([1, 2], 3) == -1, "IndexError"
