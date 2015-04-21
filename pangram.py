755ee720f33926d5a8db6c0708cd5c7fb680df21
Mauricio de Abreu Antunes <mauricio.abreua@gmail.com>
2015-04-21 16:38:05 -0300

    Added pangram
    My solution is better than winner solution. :)

---
+++ pangram.py
@@ -0,0 +1,12 @@
+import string
+​
+​
+def check_pangram(text):
+    return set(string.ascii_lowercase).issubset(set(text.lower()))
+    
+​
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert check_pangram("The quick brown fox jumps over the lazy dog."), "brown fox"
+    assert not check_pangram("ABCDEF"), "ABC"
+    assert check_pangram("Bored? Craving a pub quiz fix? Why, just come to the Royal Oak!"), "Bored?"
