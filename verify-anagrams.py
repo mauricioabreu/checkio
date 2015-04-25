1cd373b307280389283b8effb9d3355e0cd5da5d
Mauricio de Abreu Antunes <mauricio.abreua@gmail.com>
2015-04-25 12:43:32 -0300

    Added verify anagrams

---
+++ verify-anagrams.py
@@ -0,0 +1,9 @@
+def verify_anagrams(first_word, second_word):
+    return ''.join(sorted(first_word.lower().replace(' ', ''))) == ''.join(sorted(second_word.lower().replace(' ', '')))
+​
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert isinstance(verify_anagrams(u"a", u"z"), bool), "Boolean!"
+    assert verify_anagrams(u"Programming", u"Gram Ring Mop") == True, "Gram of code"
+    assert verify_anagrams(u"Hello", u"Ole Oh") == False, "Hello! Ole Oh!"
+    assert verify_anagrams(u"Kyoto", u"Tokyo") == True, "The global warming crisis of 3002"
