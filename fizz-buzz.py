0c5fbdce1a02bace98302f4973f5f611be4074a1
Mauricio de Abreu Antunes <mauricio.abreua@gmail.com>
2015-04-30 22:58:59 -0300

    Added fizz buzz
    A very clear solution.

---
+++ fizz-buzz.py
@@ -0,0 +1,17 @@
+def checkio(number):
+    if number % 15 == 0:
+        return "Fizz Buzz"
+    elif number % 5 == 0:
+        return "Buzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    else:
+        return str(number)
+
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio(15) == "Fizz Buzz", "15 is divisible by 3 and 5"
+    assert checkio(6) == "Fizz", "6 is divisible by 3"
+    assert checkio(5) == "Buzz", "5 is divisible by 5"
+    assert checkio(7) == "7", "7 is not divisible by 3 or 5"
