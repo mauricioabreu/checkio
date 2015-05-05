da51d9532dd73142e5747fd4997d273b81508c25
Mauricio de Abreu Antunes <mauricio.abreua@gmail.com>
2015-05-05 07:38:33 -0300

    Added weak point

---
+++ weak-point.py
@@ -0,0 +1,25 @@
+import itertools
+​
+​
+def weak_point(matrix):
+    horizontal_weak_point, _ = min(enumerate(map(sum, matrix)), key=lambda x: x[1])
+    vertical_weak_point, _ = min(enumerate(map(sum, itertools.izip_longest(*matrix))), key=lambda x: x[1]) 
+    return horizontal_weak_point, vertical_weak_point
+​
+​
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert isinstance(weak_point([[1]]), (list, tuple)), "The result should be a list or a tuple"
+    assert list(weak_point([[7, 2, 7, 2, 8],
+                            [2, 9, 4, 1, 7],
+                            [3, 8, 6, 2, 4],
+                            [2, 5, 2, 9, 1],
+                            [6, 6, 5, 4, 5]])) == [3, 3], "Example"
+    assert list(weak_point([[7, 2, 4, 2, 8],
+                            [2, 8, 1, 1, 7],
+                            [3, 8, 6, 2, 4],
+                            [2, 5, 2, 9, 1],
+                            [6, 6, 5, 4, 5]])) == [1, 2], "Two weak point"
+    assert list(weak_point([[1, 1, 1],
+                            [1, 1, 1],
+                            [1, 1, 1]])) == [0, 0], "Top left"
